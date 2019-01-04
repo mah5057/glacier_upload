8e7475685bfdb6cbb62e75b2c7454503d8cf6dde
mah5057 <matt@objectlabs.com>
2019-01-04 00:57:34 -0800

    Adding dry run and finishing upload command logic

--- gbs.py
+++ gbs.py
@@ -6,8 +6,6 @@ from multiprocessing import Process, Queue, current_process
 
 from glacier_upload_file import GlacierUploadFile
 
-VAULT='examplevault'
-
 # client to initialize glacier
 session = boto3.Session(profile_name='default')
 glacier_client = session.client('glacier')
@@ -18,9 +16,9 @@ glacier_client = session.client('glacier')
 
 # TODO: On resume, get uploaded parts and mark the byte range as uploaded (ByteRange.uploaded)
 
-# get a list of lists of the ByteRanges that comprise the file to divide and share amongst the workers
+# partition byte ranges to distribute to workers
 # TODO: optimize number of workers?
-def divvy_byte_ranges(byte_ranges, num_workers):
+def partition_byte_ranges(byte_ranges, num_workers):
     return [byte_ranges[x::num_workers] for x in xrange(num_workers)]
 
 def calculate_treehash_worker_process(upload_file, q):
@@ -41,12 +39,15 @@ def upload_worker_process(byte_ranges, filename, upload_id):
                                             body=f.read(byte_range.get_chunk_size()), 
                                             range=byte_range.get_range_string(), 
                                             uploadId=upload_id, 
-                                            vaultName=VAULT)
+                                            vaultName=vault)
 
 # TODO: Need an arg parser 
 # => upload (resume local file?) (file, vault, archive_description, )
 # => retrieve
 # => main()
+# => logging
+# => config file
+# => database
 
 ################################################################
 # main
@@ -55,9 +56,9 @@ def upload_worker_process(byte_ranges, filename, upload_id):
 def main(args):
     """
     need argparse stuff for:
-    rgbs upload --vault "vaultname" --description "archive description" <File>
-    rgbs get-jobs --finished --in-progress
-    rgbs retrieve --vault "vaultname"? --archive-id "archiveId"
+    gbs upload --vault "vaultname" --description "archive description" <File>
+    gbs get-jobs --finished --in-progress
+    gbs retrieve --vault "vaultname"? --archive-id "archiveId"
     argparse -- https://docs.python.org/dev/library/argparse.html#sub-commands
     """
     parser = ArgumentParser()
@@ -66,15 +67,25 @@ def main(args):
     # upload command definition
     ###########################
     upload_parser = subparsers.add_parser('upload')
-    upload_parser.add_argument('-v', '--vault', type=str, default='')
-    upload_parser.add_argument('-d', '--description', type=str, default='')
-    parser.add_argument('filepath', metavar='F', type=str, nargs='+',
-                    help='path to file to upload')
+    upload_parser.add_argument('-v', '--vault', type=str, default='',
+                    help='Target glacier vault')
+    upload_parser.add_argument('-d', '--description', type=str, default='',
+                    help='Description of archive')
+    upload_parser.add_argument('-w', '--workers', type=int, default=8,
+                    help='Maximum number of workers to deploy')
+    upload_parser.add_argument('--dry-run', action='store_true',
+                    help='Will only print byte ranges if specified')
+    upload_parser.add_argument('filepath', metavar='F', type=str, nargs='+',
+                    help='Path of file to upload')
     upload_parser.set_defaults(func=upload_command)
 
     # TODO: get-jobs command
 
     # TODO: retrieve command
+
+    # TODO: get-archives / get-vaults commands?
+
+    # TODO: create-vault
     
     arguments = parser.parse_args(args)
     arguments.func(arguments)
@@ -85,7 +96,79 @@ def main(args):
 
 def upload_command(args):
     # now parse arguments and execute upload logic
-    print args
+    vault = args.vault
+    description = args.description
+    num_workers = args.workers
+    file_path = args.filepath
+    dry_run = args.dry_run
+
+    if len(file_path) > 1:
+        raise Exception("Too many arguments.")
+    else:
+        file_path = file_path[0]
+
+    print "Preparing file for upload..."
+
+    f = GlacierUploadFile(file_path)
+    # divide byte ranges to upload for the workers
+    divided_ranges = partition_byte_ranges(f.get_parts(), num_workers)
+
+    if not dry_run:
+        print "Initializing multipart upload to Amazon Glacier..."
+
+        # initialize multipart upload
+        init_mpu_response = glacier_client.initiate_multipart_upload(
+                    accountId='-',
+                    vaultName=vault,
+                    archiveDescription='testing upload 6 GiB',
+                    partSize=str(f.get_part_size()),
+                )
+
+        upload_id = init_mpu_response['uploadId']
+
+        q = Queue()
+        treehash_p = Process(target=calculate_treehash_worker_process, args=(f, q,))
+
+        # start calculating that treehash
+        treehash_p.start()
+        
+        upload_workers = []
+        # kick off uploader threads
+        for set_of_ranges in divided_ranges:
+            if set_of_ranges:
+                p = Process(target=upload_worker_process, args=(set_of_ranges, file_path, upload_id,))
+                upload_workers.append(p)
+                p.start()
+
+        # wait for uploader and treehash calculator threads to finish
+        for worker in upload_workers:
+            worker.join()
+
+        print "Waiting for treehash calculation..."
+        treehash_p.join()
+
+        # get treehash from the q
+        treehash = q.get()
+
+        print treehash
+
+        # complete multipart upload after upload parts and treehas calculator join
+        print "Completing multipart upload..."
+        complete_mpu_response = glacier_client.complete_multipart_upload(accountId='-', 
+                                                                        vaultName=vault, 
+                                                                        uploadId=upload_id, 
+                                                                        archiveSize=str(f.get_total_size_in_bytes()), 
+                                                                        checksum=treehash)
+
+        print "\nComplete response: " + str(complete_mpu_response)
+    else:
+        print "\nProcess to byte range mappings"
+        print "------------------------------"
+        for process, set_of_ranges in enumerate(divided_ranges, 1):
+            if set_of_ranges:
+                print "\nProcess %d" % process
+                for one_range in set_of_ranges:
+                    print "    %s" % one_range.get_range_string()
 
 ################################################################
 # run main with sys args
@@ -98,61 +181,3 @@ if __name__ == '__main__':
             pass
     except:
         traceback.print_exc()
-"""
-number_of_workers = 8
-
-file_path = sys.argv[1]
-
-print "Preparing file for upload..."
-
-f = GlacierUploadFile(file_path)
-# divide byte ranges to upload for the workers
-divided_ranges = divvy_byte_ranges(f.get_parts(), number_of_workers)
-
-print "Initializing multipart upload to Amazon Glacier..."
-
-# initialize multipart upload
-init_mpu_response = glacier_client.initiate_multipart_upload(
-            accountId='-',
-            vaultName=VAULT,
-            archiveDescription='testing upload 6 GiB',
-            partSize=str(f.get_part_size()),
-        )
-
-upload_id = init_mpu_response['uploadId']
-
-q = Queue()
-treehash_p = Process(target=calculate_treehash_worker_process, args=(f, q,))
-
-# start calculating that treehash
-treehash_p.start()
-
-upload_workers = []
-# kick off uploader threads
-for set_of_ranges in divided_ranges:
-    p = Process(target=upload_worker_process, args=(set_of_ranges, file_path, upload_id,))
-    upload_workers.append(p)
-    p.start()
-
-# wait for uploader and treehash calculator threads to finish
-for worker in upload_workers:
-    worker.join()
-
-print "Waiting for treehash calculation..."
-treehash_p.join()
-
-# get treehash from the q
-treehash = q.get()
-
-# complete multipart upload after upload parts and treehas calculator join
-print "Completing multipart upload..."
-complete_mpu_response = glacier_client.complete_multipart_upload(accountId='-', 
-                                                                 vaultName=VAULT, 
-                                                                 uploadId=upload_id, 
-                                                                 archiveSize=str(f.get_total_size_in_bytes()), 
-                                                                 checksum=treehash)
-
-print "\nComplete response: " + str(complete_mpu_response)
-"""
-
-
