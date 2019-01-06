1be0e3a4855c59b80fd66f1cb09f566788f1b405
mah5057 <matt@objectlabs.com>
2019-01-05 18:51:24 -0800

    list-archives, delete-archives, shortId, groundwork for upload-archives --resume

--- gbs.py
+++ gbs.py
@@ -5,8 +5,9 @@ import traceback
 from argparse import ArgumentParser
 from multiprocessing import Process, Queue, current_process
 
-from pymongo import MongoClient
+from random import randint
 from os.path import expanduser
+from pymongo import MongoClient
 from glacier_upload_file import GlacierUploadFile
 
 """
@@ -47,6 +48,11 @@ else:
 def partition_byte_ranges(byte_ranges, num_workers):
     return [byte_ranges[x::num_workers] for x in xrange(num_workers)]
 
+def get_long_id(short_id):
+    archive_document = ARCHIVES_COLLECTION.find_one({"shortId": short_id})
+    if archive_document:
+        return archive_document["_id"]
+
 def calculate_treehash_worker_process(upload_file, q):
     print "[%s] -- calculating treehash..." % current_process().name
     q.put(upload_file.get_treehash()) 
@@ -68,12 +74,8 @@ def upload_worker_process(vault, byte_ranges, filename, upload_id):
                                             vaultName=vault)
 
 # TODO: Need an arg parser 
-# => upload (resume local file?) (file, vault, archive_description, )
 # => retrieve
-# => main()
 # => logging
-# => config file
-# => database
 
 ################################################################
 # main
@@ -82,7 +84,6 @@ def upload_worker_process(vault, byte_ranges, filename, upload_id):
 def main(args):
     """
     need argparse stuff for:
-    gbs upload --vault "vaultname" --description "archive description" <File>
     gbs get-jobs --finished --in-progress
     gbs retrieve --vault "vaultname"? --archive-id "archiveId"
     argparse -- https://docs.python.org/dev/library/argparse.html#sub-commands
@@ -90,9 +91,9 @@ def main(args):
     parser = ArgumentParser()
     subparsers = parser.add_subparsers()
 
+    # TODO: resume upload logic
     # upload command definition
-    ###########################
-    upload_parser = subparsers.add_parser('upload')
+    upload_parser = subparsers.add_parser('upload-archive')
     upload_parser.add_argument('-v', '--vault', type=str, default='',
                     help='Target glacier vault')
     upload_parser.add_argument('-d', '--description', type=str, default='',
@@ -103,11 +104,21 @@ def main(args):
                     help='Will only print byte ranges if specified')
     upload_parser.add_argument('filepath', metavar='F', type=str, nargs='+',
                     help='Path of file to upload')
-    upload_parser.set_defaults(func=upload_command)
+    upload_parser.set_defaults(func=upload_archive_command)
 
-    # TODO: delete-archive commmand
+    # list-archives command definition
+    list_archives_parser = subparsers.add_parser('list-archives')
+    list_archives_parser.add_argument('-v', '--vault', type=str, default='',
+                            help='Specify vault from which to display archives list')
+    list_archives_parser.set_defaults(func=list_archives)
 
-    # TODO: get-archives command
+    # delete-archive commmand
+    delete_archives_parser = subparsers.add_parser('delete-archive')
+    delete_archives_parser.add_argument('-v', '--vault', type=str, default='',
+                            help='Target glacier vault')
+    delete_archives_parser.add_argument('-i', '--shortId', type=str, default='',
+                            help='Target archive id')
+    delete_archives_parser.set_defaults(func=delete_archive_command)
 
     # TODO: get-vaults command
 
@@ -116,6 +127,8 @@ def main(args):
     # TODO: retrieve command
 
     # TODO: create-vault command
+
+    # TODO: get-long-id command
     
     arguments = parser.parse_args(args)
     arguments.func(arguments)
@@ -124,8 +137,75 @@ def main(args):
 # sub command methods
 ################################################################
 
-def upload_command(args):
-    # now parse arguments and execute upload logic
+def list_archives(args):
+    # TODO: make this more maintainable
+    vault = args.vault
+
+    header = "ID                    description                            vault"
+    print header
+    print "-" * (len(header) + 20)
+    if ARCHIVES_COLLECTION:
+        rows = []
+        longest_row = len(header)
+        
+        if vault:
+            archives = ARCHIVES_COLLECTION.find({"vault": vault,
+                                                "deleted": {"$exists": False}})
+        else:
+            archives = ARCHIVES_COLLECTION.find({"deleted": {"$exists": False}})
+
+        for archive in archives:
+            short_id = archive["shortId"]
+            description = archive["description"]
+            vault = archive["vault"]
+
+            display_si = short_id[:18]
+            display_description = description[:34]
+            display_vault = vault[:21]
+
+            display_si = display_si + "..." if len(display_si) < len(short_id) else display_si
+            display_description = display_description + "..." if len(display_description) < len(description) else description
+            display_vault = display_vault + "..." if len(display_vault) < len(vault) else display_vault
+
+            row =  "%s%s%s%s%s%s" % (display_si, " " * (22 - len(display_si)), 
+                                     display_description, " " * (39 - len(display_description)), 
+                                     display_vault, " " * (25 - len(display_vault)))
+            
+            rows.append(row)
+
+        for row in rows:
+            print row
+    else:
+        raise Exception("DB REQUIRED")
+
+def delete_archive_command(args):
+    vault = args.vault
+    short_id = args.shortId
+
+    _id = get_long_id(short_id)
+    
+    if not vault:
+        if ARCHIVES_COLLECTION:
+            vault = ARCHIVES_COLLECTION.find_one({"_id": _id})['vault']
+        else:
+            raise Exception("DB OR VAULT NAME REQUIRED: cannot determine vault without name.")
+
+    session = boto3.Session(profile_name='default')
+    glacier_client = session.client('glacier')
+
+    delete_response = glacier_client.delete_archive(accountId='-',
+                                                    archiveId=_id,
+                                                    vaultName=vault,)
+
+    ARCHIVES_COLLECTION.update_one({"_id": _id}, {"$set": {"deleted": True}}, upsert=False)
+
+    if delete_response['ResponseMetadata']['HTTPStatusCode'] in [200, 202, 204]:
+        print "\nSuccessfully deleted archive w/ id: %s from vault: %s!!\n" % (short_id, vault)
+    else:
+        print "\nFailed to delete archive, response:\n"
+        print deleted_response
+
+def upload_archive_command(args):
     vault = args.vault
     description = args.description
     num_workers = args.workers
@@ -144,16 +224,16 @@ def upload_command(args):
 
     f = GlacierUploadFile(file_path)
     # divide byte ranges to upload for the workers
-    divided_ranges = partition_byte_ranges(f.get_parts(), num_workers)
+    partitioned_ranges = partition_byte_ranges(f.get_parts(), num_workers)
 
     if not dry_run:
-        print "Initializing multipart upload to Amazon Glacier..."
+        print "Initializing multipart upload to Amazon Glacier...\n"
 
         # initialize multipart upload
         init_mpu_response = glacier_client.initiate_multipart_upload(
                     accountId='-',
                     vaultName=vault,
-                    archiveDescription='testing upload 6 GiB',
+                    archiveDescription=description,
                     partSize=str(f.get_part_size()),
                 )
 
@@ -167,7 +247,7 @@ def upload_command(args):
         
         upload_workers = []
         # kick off uploader threads
-        for set_of_ranges in divided_ranges:
+        for set_of_ranges in partitioned_ranges:
             if set_of_ranges:
                 p = Process(target=upload_worker_process, args=(vault, set_of_ranges, file_path, upload_id,))
                 upload_workers.append(p)
@@ -177,16 +257,16 @@ def upload_command(args):
         for worker in upload_workers:
             worker.join()
 
-        print "Waiting for treehash calculation..."
+        print "\nWaiting for treehash calculation..."
         treehash_p.join()
 
         # get treehash from the q
         treehash = q.get()
 
-        print treehash
+        print "\nTREEHASH: %s" % treehash
 
         # complete multipart upload after upload parts and treehas calculator join
-        print "Completing multipart upload..."
+        print "\nCompleting multipart upload..."
         complete_mpu_response = glacier_client.complete_multipart_upload(accountId='-', 
                                                                         vaultName=vault, 
                                                                         uploadId=upload_id, 
@@ -194,22 +274,25 @@ def upload_command(args):
                                                                         checksum=treehash)
 
         if ARCHIVES_COLLECTION:
-            print "Saving to database..."
+            se_index = randint(1,15)
+            short_id = complete_mpu_response['archiveId'][se_index:se_index + 15]
+            print "\nWriting document with ID: %s to database..." % short_id
             archive_doc = {
                 "_id": complete_mpu_response['archiveId'],
+                "shortId": short_id,
                 "description": description,
                 "vault": vault,
                 "checksum": complete_mpu_response['checksum'],
                 "location": complete_mpu_response['location']
             }
             ARCHIVES_COLLECTION.insert(archive_doc)
-            print "\nInserted to database: " + str(archive_doc)
+            print "\nWritten to database: %s\n" % str(archive_doc)
         else:
-            print "\nComplete response: " + str(complete_mpu_response)
+            print "\nComplete response: %s\n" % str(complete_mpu_response)
     else:
         print "\nProcess to byte range mappings"
         print "------------------------------"
-        for process, set_of_ranges in enumerate(divided_ranges, 1):
+        for process, set_of_ranges in enumerate(partitioned_ranges, 1):
             if set_of_ranges:
                 print "\nProcess %d" % process
                 for one_range in set_of_ranges:
