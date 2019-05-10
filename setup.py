3f00cd430dddb1419ec5589289b0a6cc0f8dca05
matt <mah5057@gmail.com>
2019-05-09 22:43:12 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='agbus',  
-     version='0.1',
+     version='0.2',
      scripts=['bin/agbus'] ,
      author="Matt Hardock",
      author_email="mah5057@gmail.com",
