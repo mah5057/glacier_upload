aaf3da72f932198bb10829f844974c160e9f7fe5
matt <mah5057@gmail.com>
2019-05-09 22:20:06 -0700

    Making into a pip package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='agbus',  
+     version='0.1',
+     scripts=['bin/agbus'] ,
+     author="Matt Hardock",
+     author_email="mah5057@gmail.com",
+     description="A simplified Amazon Glacier cli, integrated with a database.",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="http://www.matthardock.com/",
+     packages=setuptools.find_packages(),
+     install_requires=[
+        'boto3==1.9.50',
+        'pymongo==3.7.2'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
